c4a34d23883424c17caae5fc885de25c405c9178
Gagan Vats <gaganvats05@gmail.com>
2022-05-05 14:03:28 +0530

    refactored landing page UI

---
+++ src/utils.js
@@ -0,0 +1,5 @@
+import { Responsive } from "semantic-ui-react";
+export const getWidth = () => {
+  const isSSR = typeof window === "undefined";
+  return isSSR ? Responsive.onlyTablet.minWidth : window.innerWidth;
+};
